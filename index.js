29577895fbb3bfe8dfc13a4d899663dd37822ed1
sai karthik ketha <69042752+saiisback@users.noreply.github.com>
2025-01-14 11:03:33 +0000

    update 0.8

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = 3001;
+
+// Middleware for CORS
+app.use(cors({
+  origin: "https://margrosgames.vercel.app/register", // Allow requests only from https://margrosgames.vercel.app/
+  methods: "GET,POST,PUT,DELETE", // Allow specific HTTP methods
+  allowedHeaders: "Content-Type, Authorization", // Allow specific headers
+}));
 
 // Middleware
 app.use(bodyParser.json());
@@ -11,4 +19,4 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api', userRoutes);
 
-app.listen(PORT, () => console.log(`Server running on https://margrosgames.vercel.app/${PORT}`));
+app.listen(PORT, () => console.log(`Server running on  https://margrosgames.vercel.app/${PORT}`));
